bc3a37759c92bc76bcffe792d8cab5978d03ae55
James Christopher <jamescorpuz88@gmail.com>
2023-09-24 15:44:24 +0800

    Modified PopularJobs, and Welcome components

--- ThesisApp/app/_layout.js
+++ ThesisApp/app/_layout.js
@@ -1,7 +1,31 @@
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { View, Text } from 'react-native'; // Import necessary components
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    return <Stack />
+    // LOAD FONT START
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf')
+    })
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded])
+
+    if (!fontsLoaded) {
+        return null; // Return null while fonts are loading
+    }
+    // LOAD FONT END
+
+    return <Stack onLayout={onLayoutRootView} />
 }
 
 export default Layout;
